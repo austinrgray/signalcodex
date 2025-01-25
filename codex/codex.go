20cef3944996fd8fca7bd6c2e4b33601f82ce2e1
Austin Gray <austinrgray@protonmail.com>
2025-01-24 18:02:17 -0600

    added logs/codex for receiver and mission control

--- codex/codex.go
+++ codex/codex.go
@@ -13,26 +13,37 @@ const (
 type SubName string
 
 const (
-	SubRelayEmitterAll              SubName = SubName(SvcRelayEmitter + ".>")
-	SubRelayEmitterFleet            SubName = SubName(SvcRelayEmitter + ".*")
-	SubRelayEmitterFleetCommand     SubName = SubName(SvcRelayEmitter + ".*.command")
-	SubRelayEmitterVesselFmtCommand SubName = SubName(SvcRelayEmitter + ".%s.command")
+	SubRelayEmitterAll                 SubName = SubName(SvcRelayEmitter + ".>")
+	SubRelayEmitterFleet               SubName = SubName(SvcRelayEmitter + ".*")
+	SubRelayEmitterFleetCommand        SubName = SubName(SvcRelayEmitter + ".*.command")
+	SubRelayEmitterVesselFmtCommand    SubName = SubName(SvcRelayEmitter + ".%s.command")
+	SubRelayReceiverAll                SubName = SubName(SvcRelayReceiver + ".>")
+	SubRelayReceiverFleet              SubName = SubName(SvcRelayReceiver + ".*")
+	SubRelayReceiverFleetCommand       SubName = SubName(SvcRelayReceiver + ".*.status")
+	SubRelayReceiverVesselFmtCommand   SubName = SubName(SvcRelayReceiver + ".%s.status")
+	SubMissionControlAll               SubName = SubName(SvcMissionControl + ".>")
+	SubMissionControlSentryFleetStatus SubName = SubName(SvcMissionControl + ".sentry.fleet.status")
 )
 
 type StreamName string
 
 const (
-	StreamRelayEmitter StreamName = StreamName(SvcRelayEmitter + "-stream")
+	StreamRelayEmitter   StreamName = StreamName(SvcRelayEmitter + "-stream")
+	StreamRelayReceiver  StreamName = StreamName(SvcRelayReceiver + "-stream")
+	StreamMissionControl StreamName = StreamName(SvcMissionControl + "-stream")
 )
 
 type MicroName string
 
 const (
-	MicroRelayEmitter MicroName = MicroName(SvcRelayEmitter + "-service")
+	MicroRelayEmitter   MicroName = MicroName(SvcRelayEmitter + "-service")
+	MicroMissionControl MicroName = MicroName(SvcMissionControl + "-service")
 )
 
 type EndPointName string
 
 const (
-	EPRelayEmitCommand EndPointName = EndPointName("queue-emit-command")
+	EPRelayEmitCommand        EndPointName = EndPointName("queue-emit-command")
+	EPRelayEmitAddConsumer    EndPointName = EndPointName("add-consumer")
+	EPRelayEmitRemoveConsumer EndPointName = EndPointName("remove-consumer")
 )
